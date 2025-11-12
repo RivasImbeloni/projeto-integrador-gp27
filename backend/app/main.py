bfde2aaa7fc8441fe9a895ebe78c83498623d6db
Douglas Barros <douglas.barros2000@outlook.com>
2025-11-12 00:29:58 -0300

    Fix: Correções para backend lidar com front

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,7 +6,7 @@ app = FastAPI(title="API Metas KPIs")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # origem do frontend
+    allow_origins=["http://localhost:3000"],  # origem do frontend
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
